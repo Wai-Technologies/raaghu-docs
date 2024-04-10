70a0006c25705cf45a4b6462d22e9f7165205917
Bhavani Shankar <bhavanishankar.vayalada@gmail.com>
2024-04-10 15:15:23 +0530

    Added baseUrl in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: 'https://raaghudocs.azurewebsites.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  //baseUrl: '/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
